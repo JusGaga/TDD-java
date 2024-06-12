dd7e0e5e1073f780af300232a1b8be6e7a265422
Gaetan Jus <gaetan3107@gmail.com>
2024-06-12 12:20:58 +0200

    Premiere étape: modification de la fonction rechercher afin de satisfaire le test le mot ne doit pas contenir moins de 2 caractères

--- src/main/java/org/example/exercice4/RechercheVille.java
+++ src/main/java/org/example/exercice4/RechercheVille.java
@@ -13,6 +13,9 @@ public class RechercheVille {
     }
 
     public List<String> rechercher(String mot) throws ExecutionControl.NotImplementedException {
+        if (mot.length() < 2){
+            throw new NotFoundException("Le texte de recherche doit contenir au moins 2 caractères.");
+        }
         throw new ExecutionControl.NotImplementedException("");
     }
 }
