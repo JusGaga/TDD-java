806a73ab0c83e929240045da05a7e33ebfa7d891
Gaetan Jus <gaetan3107@gmail.com>
2024-06-12 12:01:05 +0200

    first commit for tdd

---
+++ src/main/java/org/example/exercice4/RechercheVille.java
@@ -0,0 +1,18 @@
+package org.example.exercice4;
+
+import jdk.jshell.spi.ExecutionControl;
+
+import java.util.List;
+
+public class RechercheVille {
+
+    private List<String> villes;
+
+    public RechercheVille(List<String> villes) {
+        this.villes = villes;
+    }
+
+    public List<String> rechercher(String mot) throws ExecutionControl.NotImplementedException {
+        throw new ExecutionControl.NotImplementedException("");
+    }
+}
